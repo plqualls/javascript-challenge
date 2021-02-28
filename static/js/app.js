00de385d9306d784b8ad6666cee0f9e9eea4c274
Phillecia Qualls <plqualls@msn.com>
2021-02-27 22:39:57 -0600

    Updated file. Code works

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,48 @@
 // from data.js
 var tableData = data;
 
-//Creating columns for table
+// Creating References
 var $tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
-var inputDate = d3.select("#datetime");
-var inputCity = d3.select("#city");
-var columns = ["datetime", "city", "state", "country", "shape", "duration", "comments" ]
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-//Input data into HTML
+// Input data into HTML
 var addData = (dataInput) => {
-    dataInput.forEach(ufosightings => {
+    dataInput.forEach(ufoSightings => {
         var row = $tbody.append("tr");
-        columns.forEach(column => row.append("td").text(ufosightings[column])
-            )
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
     });
 }
-
+//adding data from table
 addData(tableData);
 
+// Setting up the Filter Button for Date and City
 button.on("click", () => {
+
     d3.event.preventDefault();
 
-    var inputDate = inputDate.property("value").trim();
-
+    var inputDate = inputFieldDate.property("value").trim();
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
     
+    $tbody.html("");
+
+//Loop statement
+    let response = {
+        filterDate
+    }
+
+
+    if(response.filterDate.length !== 0) {
+        addData(filterDate);
+    }
+
+    // Top if only works for filtering the date
+    
+        else {
+            $tbody.append("tr").append("td").text("NO UFO HERE!!!! Keep Searching...");
+        }
 })
+
