b311201b80d4b1a3d172288831f391c2b074623b
Phillecia Qualls <plqualls@msn.com>
2021-02-27 20:44:44 -0600

    Columns created for table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//Creating columns for table
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputDate = d3.select("#datetime");
+var inputCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "duration", "comments" ]
+
+//Input data into HTML
+var addData = (dataInput) => {
+    dataInput.forEach(ufosightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufosightings[column])
+            )
+    });
+}
+
